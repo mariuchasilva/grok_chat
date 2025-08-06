315d0971b15d3ce08e04697074005597b0060a5c
mariuchasilva <78319598+mariuchasilva@users.noreply.github.com>
2025-08-06 12:39:19 -0300

    melhorias

--- grok_chat.py
+++ grok_chat.py
@@ -1,19 +1,27 @@
 import os
 from groq import Groq
 
-client = Groq(
-    api_key=os.environ.get('GROQ_API_KEY')
-)
+def interacao_chat(pergunta):
+    client = Groq(
+        api_key=os.environ.get('GROQ_API_KEY')
+    )
 
-pergunta = input("Digite a sua pergunta\n")
+    print("ola")
+    chat = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": pergunta
+            }
+        ],
+        model="llama-3.3-70b-versatile"
+    )
+    return chat.choices[0].message.content
 
-chat = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": pergunta
-        }
-    ],
-    model="llama-3.3-70b-versatile"
-)
-print(chat.choices[0].message.content)
+if __name__ == "__main__":
+    while True:
+        pergunta = input("Digite a sua pergunta (digite 'sair' para encerrar)\n")
+        if pergunta == 'sair':
+            print('encerrando...\n\n')
+            break
+        print(interacao_chat(pergunta))
