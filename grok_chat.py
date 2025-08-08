9bdcbf0138d4e57f9be8f30f5ba97fe881879723
mariuchasilva <78319598+mariuchasilva@users.noreply.github.com>
2025-08-08 11:26:39 -0300

    interativo

--- grok_chat.py
+++ grok_chat.py
@@ -1,27 +1,45 @@
 import os
 from groq import Groq
 
-def interacao_chat(pergunta):
+def interacao_chat():
     client = Groq(
         api_key=os.environ.get('GROQ_API_KEY')
     )
 
-    print("ola")
-    chat = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": pergunta
-            }
-        ],
-        model="llama-3.3-70b-versatile"
-    )
-    return chat.choices[0].message.content
+    conversation = [
+        {
+            "role": "system",
+            "content": "me deseje boas vindas com uma mensagem super curta"
+        }
+    ]
 
-if __name__ == "__main__":
     while True:
         pergunta = input("Digite a sua pergunta (digite 'sair' para encerrar)\n")
         if pergunta == 'sair':
             print('encerrando...\n\n')
             break
-        print(interacao_chat(pergunta))
+    
+        conversation.append(
+            {
+                "role": "user",
+                "content": pergunta
+            }
+        )
+
+        chat = client.chat.completions.create(
+            messages=conversation,
+            model="llama-3.3-70b-versatile"
+        )
+        ai_response = chat.choices[0].message.content
+
+        conversation.append(
+            {
+                "role": "assistent",
+            "content": ai_response  
+            }
+        )
+
+        print(ai_response)
+
+if __name__ == "__main__":
+    interacao_chat()
